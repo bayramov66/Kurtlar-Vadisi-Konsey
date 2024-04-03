9f027e08bf7f573af0b22e8e92bf7ba2dd15a077
bayramov66 <kamran111.kb@gmail.com>
2024-04-03 08:22:37 -0800

    KV konsey uyeleri

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import "./App.css";
+import Data from "./Data.json";
+
+function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  return (
+    <div className="container">
+      <h1>Kurtlar Vadisi Konsey Üyeleri</h1>
+      <div className="inputContainer">
+        <input
+          className="search"
+          type="text"
+          placeholder="Konsey üyesi giriniz..."
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+      <div className="dataContainer">
+        {Data.filter((val) => {
+          if (searchTerm == "") return val;
+          else if (val.title.toLowerCase().includes(searchTerm.toLowerCase())) {
+            return val;
+          }
+        }).map((val) => {
+          return (
+            <div className="data" key={val.id}>
+              <img src={val.image} alt="" />
+              <h3>{val.title}</h3>
+              <p>{val.Description}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
